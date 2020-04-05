067a7a7c19ecc8c4b964b1cd5cb0c37ec3ee69cc
Oussama <oussamaerrahem@gmail.com>
2020-04-05 19:11:25 +0200

    Updates

---
+++ precache-manifest.9dafe6dc721b3586048a524a31a95724.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d800155426c11e8e88c7c320b81ec0fb",
+    "url": "/smartdevelopment-/index.html"
+  },
+  {
+    "revision": "bd89f7d00c3fae1594f4",
+    "url": "/smartdevelopment-/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f05158bd7396d10ba2dc",
+    "url": "/smartdevelopment-/static/css/main.4992680f.chunk.css"
+  },
+  {
+    "revision": "bd89f7d00c3fae1594f4",
+    "url": "/smartdevelopment-/static/js/2.5df0223d.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/smartdevelopment-/static/js/2.5df0223d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05158bd7396d10ba2dc",
+    "url": "/smartdevelopment-/static/js/main.159d09e7.chunk.js"
+  },
+  {
+    "revision": "4081b12c46bec6f188d5",
+    "url": "/smartdevelopment-/static/js/runtime-main.51c04d8f.js"
+  },
+  {
+    "revision": "020c9eb834982825b30175d37a0d17d0",
+    "url": "/smartdevelopment-/static/media/about-bg.020c9eb8.jpg"
+  },
+  {
+    "revision": "6b1d7b9249e7cdf3d86b567d3032115a",
+    "url": "/smartdevelopment-/static/media/home-bg.6b1d7b92.jpg"
+  },
+  {
+    "revision": "dced86d796d2e922d243f260997af4be",
+    "url": "/smartdevelopment-/static/media/image1.dced86d7.jpg"
+  },
+  {
+    "revision": "e72c2187023a4f81e5beb99fc01e51f9",
+    "url": "/smartdevelopment-/static/media/image1.e72c2187.jpg"
+  },
+  {
+    "revision": "178ffa234e75dfd850b04b650bd773c8",
+    "url": "/smartdevelopment-/static/media/image2.178ffa23.jpg"
+  },
+  {
+    "revision": "1aebbc39aa3d0f6715fa3e99b732cd65",
+    "url": "/smartdevelopment-/static/media/image3.1aebbc39.jpg"
+  },
+  {
+    "revision": "91f85a2bf75b457555982a3743ca76c3",
+    "url": "/smartdevelopment-/static/media/image3.91f85a2b.jpg"
+  },
+  {
+    "revision": "03b826c600c300db9d78b22726166a33",
+    "url": "/smartdevelopment-/static/media/image4.03b826c6.jpg"
+  },
+  {
+    "revision": "08c9320c5e5181f30d939a6dc5b7c6d1",
+    "url": "/smartdevelopment-/static/media/image5.08c9320c.jpg"
+  },
+  {
+    "revision": "3cd14e36160e75f1e4c2a960bc85423c",
+    "url": "/smartdevelopment-/static/media/image6.3cd14e36.jpg"
+  },
+  {
+    "revision": "1751e3ccf93843a345010795fc79dcd4",
+    "url": "/smartdevelopment-/static/media/image7.1751e3cc.jpg"
+  },
+  {
+    "revision": "bd46a53097c1c0cc4d916c2a724c6627",
+    "url": "/smartdevelopment-/static/media/image8.bd46a530.jpg"
+  },
+  {
+    "revision": "4cdb68be3eb8514f5bffd6c5db78ce59",
+    "url": "/smartdevelopment-/static/media/js.4cdb68be.jpeg"
+  }
+]);
